9fa537bde2b20369c8857c88cadaba6285f01074
fiekzz <fikrichuck@gmail.com>
2025-01-20 14:09:15 +0800

    ✨ Refactor page component to use HandleVoting component and ABI JSON file

--- app/voting/page.tsx
+++ app/voting/page.tsx
@@ -1,54 +1,34 @@
-"use client"
 import Candidate from '@/components/Candidate'
+import HandleVoting from '@/components/HandleVoting'
+import supabase from '@/utils/supabase'
 import { Button } from '@nextui-org/react'
 import React, { useState } from 'react'
+import { promises as fs } from 'fs';
+import abiFile from '../abi.json'
 
-interface ICandidate {
-    name: string
-    picUrl: string
+export interface People {
+    id: number;
+    name: string;
+    pictureUrl: string;
 }
 
-function Voting() {
+export interface ICandidateItem {
+    people: People
+}
 
-    const candidates: ICandidate[] = [
-        {
-            name: "Amin",
-            picUrl: "https://picsum.photos/200"
-        },
-        {
-            name: "Afiq",
-            picUrl: "https://picsum.photos/200"
-        },
-        {
-            name: "Aminah",
-            picUrl: "https://picsum.photos/200"
-        },
-    ]
+async function Voting() {
 
-    // const [selected, setSelected] = useState(false)
+    const { data, error } = await supabase.from('candidates').select('people (id, name, pictureUrl)')
 
-    const [selectedCandidate, setSelectedCandidate] = useState(-1)
+    const Candidates = data as ICandidateItem[] | null
 
-  return (
-    <div className='bg-white w-screen h-auto py-7 lg:py-0 lg:h-screen flex justify-center items-center text-black'>
-        <div className='max-w-screen-md w-full flex flex-col justify-center items-center'>
-            <h4 className='text-2xl font-medium mb-4'>Candidates</h4>
-            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
-                {candidates.map((item, index) => {
-                    return (
-                        <div key={index} className='hover:cursor-pointer' onClick={() => {
-                            setSelectedCandidate(index)
-                        }}>
-                            <Candidate candidateName={item.name} candidatePicUrl={item.picUrl} onSelected={index === selectedCandidate} />
-                        </div>
-                    )
-                })}
-            </div>
-            <h4 className='text-xl font-medium mt-6 mb-4'>Selected candidate: {selectedCandidate === -1 ? "" : candidates[selectedCandidate].name}</h4>
-            <Button color='primary' className='w-[200px]'>Vote</Button>
-        </div>
-    </div>
-  )
+    const file = await fs.readFile(process.cwd() + '/app/abi.json', 'utf8');
+
+    const abiData = JSON.parse(file)
+    
+    return (
+        <HandleVoting candidates={Candidates!} file={abiData}/>
+    )
 }
 
 export default Voting
